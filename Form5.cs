6f86c296640f5fa4cd53bdc30e963788550bd0dc
2203399 <2203399@uad.ac.uk>
2023-12-14 18:32:02 +0000

    added some combobox functionality

--- Form5.cs
+++ Form5.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,7 +13,7 @@ namespace WindowsFormsApp1
 {
     public partial class Form5 : Form
     {
-        public string connectionString = "server=lochnagar.abertay.ac.uk;uid=sql2203399;pwd=oJ8HnTiBzB68;database=sql2203399;"
+        public string connectionString = "server=lochnagar.abertay.ac.uk;uid=sql2203399;pwd=oJ8HnTiBzB68;database=sql2203399;";
         public Form5()
         {
             InitializeComponent();
@@ -27,11 +28,58 @@ namespace WindowsFormsApp1
         {
             if (hsAsset_ComboBox.SelectedIndex == 0)
             {
+                hsAsset_ComboBox.Visible = false;
+                asset_ComboBox.Visible = true;
+                string query = "SELECT name FROM HardwareAssets";
 
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    using (MySqlCommand command = new MySqlCommand(query, connection))
+                    {
+                        using (MySqlDataReader reader = command.ExecuteReader())
+                        {
+                            if (reader.HasRows)
+                            {
+                                while (reader.Read())
+                                {
+                                    string item = reader.GetString(0);
+                                    asset_ComboBox.Items.Add(item);
+                                }
+                            }
+                        }
+
+                    }
+                }
             }
 
             else if (hsAsset_ComboBox.SelectedIndex == 1)
             {
+                hsAsset_ComboBox.Visible = false;
+                asset_ComboBox.Visible = true;
+                string query = "SELECT name FROM SoftwareAssets";
+
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    using (MySqlCommand command = new MySqlCommand(query, connection))
+                    {
+                        using (MySqlDataReader reader = command.ExecuteReader())
+                        {
+                            if (reader.HasRows)
+                            {
+                                while (reader.Read())
+                                {
+                                    string item = reader.GetString(0);
+                                    asset_ComboBox.Items.Add(item);
+                                }
+                            }
+                        }
+
+                    }
+                }
 
             }
         }
