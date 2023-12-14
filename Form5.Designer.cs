5332adce23e0c1949faeb8514b3b00feed6441c4
2203399 <2203399@uad.ac.uk>
2023-12-14 17:46:04 +0000

    added all the assets and now starting to add functionality

--- Form5.Designer.cs
+++ Form5.Designer.cs
@@ -28,12 +28,247 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.asset_ComboBox = new System.Windows.Forms.ComboBox();
+            this.editSoftwarePanel = new System.Windows.Forms.Panel();
+            this.edit_Button = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.hsAsset_ComboBox = new System.Windows.Forms.ComboBox();
+            this.assetEdit_Lbl = new System.Windows.Forms.Label();
+            this.editSoftwarePanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // asset_ComboBox
+            // 
+            this.asset_ComboBox.FormattingEnabled = true;
+            this.asset_ComboBox.Location = new System.Drawing.Point(15, 148);
+            this.asset_ComboBox.Name = "asset_ComboBox";
+            this.asset_ComboBox.Size = new System.Drawing.Size(121, 21);
+            this.asset_ComboBox.TabIndex = 0;
+            this.asset_ComboBox.Visible = false;
+            // 
+            // editSoftwarePanel
+            // 
+            this.editSoftwarePanel.Controls.Add(this.textBox10);
+            this.editSoftwarePanel.Controls.Add(this.textBox9);
+            this.editSoftwarePanel.Controls.Add(this.textBox8);
+            this.editSoftwarePanel.Controls.Add(this.textBox7);
+            this.editSoftwarePanel.Controls.Add(this.textBox6);
+            this.editSoftwarePanel.Controls.Add(this.textBox5);
+            this.editSoftwarePanel.Controls.Add(this.textBox4);
+            this.editSoftwarePanel.Controls.Add(this.textBox3);
+            this.editSoftwarePanel.Controls.Add(this.textBox2);
+            this.editSoftwarePanel.Controls.Add(this.textBox1);
+            this.editSoftwarePanel.Controls.Add(this.label7);
+            this.editSoftwarePanel.Controls.Add(this.label6);
+            this.editSoftwarePanel.Controls.Add(this.label5);
+            this.editSoftwarePanel.Controls.Add(this.label3);
+            this.editSoftwarePanel.Controls.Add(this.label1);
+            this.editSoftwarePanel.Location = new System.Drawing.Point(236, 66);
+            this.editSoftwarePanel.Name = "editSoftwarePanel";
+            this.editSoftwarePanel.Size = new System.Drawing.Size(444, 321);
+            this.editSoftwarePanel.TabIndex = 1;
+            // 
+            // edit_Button
+            // 
+            this.edit_Button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.edit_Button.Location = new System.Drawing.Point(15, 364);
+            this.edit_Button.Name = "edit_Button";
+            this.edit_Button.Size = new System.Drawing.Size(75, 23);
+            this.edit_Button.TabIndex = 2;
+            this.edit_Button.Text = "Edit";
+            this.edit_Button.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "label3";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(26, 147);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(26, 210);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(26, 269);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "label7";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(90, 33);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(90, 90);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 8;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(90, 156);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 9;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(90, 210);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 10;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(90, 269);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 11;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(249, 33);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 12;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(249, 89);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 20);
+            this.textBox7.TabIndex = 13;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(249, 155);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 20);
+            this.textBox8.TabIndex = 14;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(249, 209);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(100, 20);
+            this.textBox9.TabIndex = 15;
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(249, 268);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(100, 20);
+            this.textBox10.TabIndex = 16;
+            // 
+            // hsAsset_ComboBox
+            // 
+            this.hsAsset_ComboBox.FormattingEnabled = true;
+            this.hsAsset_ComboBox.Items.AddRange(new object[] {
+            "Hardware",
+            "Software"});
+            this.hsAsset_ComboBox.Location = new System.Drawing.Point(15, 99);
+            this.hsAsset_ComboBox.Name = "hsAsset_ComboBox";
+            this.hsAsset_ComboBox.Size = new System.Drawing.Size(121, 21);
+            this.hsAsset_ComboBox.TabIndex = 3;
+            this.hsAsset_ComboBox.SelectedIndexChanged += new System.EventHandler(this.hsAsset_ComboBox_SelectedIndexChanged);
+            // 
+            // assetEdit_Lbl
+            // 
+            this.assetEdit_Lbl.AutoSize = true;
+            this.assetEdit_Lbl.Location = new System.Drawing.Point(12, 66);
+            this.assetEdit_Lbl.Name = "assetEdit_Lbl";
+            this.assetEdit_Lbl.Size = new System.Drawing.Size(174, 13);
+            this.assetEdit_Lbl.TabIndex = 4;
+            this.assetEdit_Lbl.Text = "Select which asset you want to edit";
+            // 
+            // Form5
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.asset_ComboBox);
+            this.Controls.Add(this.assetEdit_Lbl);
+            this.Controls.Add(this.editSoftwarePanel);
+            this.Controls.Add(this.edit_Button);
+            this.Controls.Add(this.hsAsset_ComboBox);
+            this.Name = "Form5";
             this.Text = "Form5";
+            this.Load += new System.EventHandler(this.Form5_Load);
+            this.editSoftwarePanel.ResumeLayout(false);
+            this.editSoftwarePanel.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox asset_ComboBox;
+        private System.Windows.Forms.Panel editSoftwarePanel;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button edit_Button;
+        private System.Windows.Forms.ComboBox hsAsset_ComboBox;
+        private System.Windows.Forms.Label assetEdit_Lbl;
     }
 }
--- Form5.cs
+++ Form5.cs
@@ -12,9 +12,28 @@ namespace WindowsFormsApp1
 {
     public partial class Form5 : Form
     {
+        public string connectionString = "server=lochnagar.abertay.ac.uk;uid=sql2203399;pwd=oJ8HnTiBzB68;database=sql2203399;"
         public Form5()
         {
             InitializeComponent();
+        }
+
+        private void Form5_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void hsAsset_ComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (hsAsset_ComboBox.SelectedIndex == 0)
+            {
+
+            }
+
+            else if (hsAsset_ComboBox.SelectedIndex == 1)
+            {
+
+            }
         }
     }
 }
